098f167d48d58d8c4721e3056c519c63929669db
Edumacsou <edu.maciel@yahoo.com.br>
2022-08-08 23:31:44 -0300

    Caminho relativo corrigido

--- app/src/main/main.py
+++ app/src/main/main.py
@@ -1,10 +1,14 @@
 from antlr4 import *
+import os
+ROOT_PATH = os.path.normpath(os.path.join(os.path.dirname(__file__), '../../'))
+import sys
+sys.path.append(ROOT_PATH)
 from src.parser.IsiLangLexer import IsiLangLexer
 from src.parser.IsiLangParser import IsiLangParser
 
 
 def main():
-    input_txt = FileStream('../../input.txt')
+    input_txt = FileStream(f'{ROOT_PATH}/input.txt')
     lexer = IsiLangLexer(input_txt)
     stream = CommonTokenStream(lexer)
     parser = IsiLangParser(stream)
