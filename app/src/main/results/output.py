5e9a99954071a87ccd0a2cd888411fa7922d58bc
henriquefsa98 <60986807+henriquefsa98@users.noreply.github.com>
2022-08-19 21:12:31 -0300

    .

--- app/src/main/results/output.py
+++ app/src/main/results/output.py
@@ -4,15 +4,14 @@ def main():
     b = input()
     a = 1+2*3/b
     c = 5.755
+    
     if(a>b):
         print(a)
     else:
         print(b)
-    while(b<a):
+    while(a<b):
        a = a+1
        print(a)
-       while(b<a):
-   print(b)
 
 if __name__ == "__main__":
     main()
